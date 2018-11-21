0c3aea6496a984bf1797276dcd3c40192458645d
mawrkus <web@sparring-partner.be>
2018-11-21 03:09:38 +0100

    Removed unnecessary promise creation in route middleware (server demo with x-request-id)

--- demos/server/middlewares.js
+++ demos/server/middlewares.js
@@ -7,19 +7,17 @@ module.exports = [
     name: 'set-request-id',
     register(server) {
       server.ext('onRequest', (request, h) => {
-        return new Promise((resolve) => {
-          request.id = requestId++; // eslint-disable-line no-plusplus
+        request.id = requestId++; // eslint-disable-line no-plusplus
 
-          h.asyncContextStore.log(`Executing server.ext.onRequest() -> new request.id=${request.id}`);
+        h.asyncContextStore.log(`Executing server.ext.onRequest() -> new request.id=${request.id}`);
 
-          h.asyncContextStore
-            .set('request.id', request.id)
-            .set('request.ua', request.headers['user-agent']);
+        h.asyncContextStore
+          .set('request.id', request.id)
+          .set('request.ua', request.headers['user-agent']);
 
-          h.asyncContextStore.log(`# contexts in store=${h.asyncContextStore.size}`);
+        h.asyncContextStore.log(`# contexts in store=${h.asyncContextStore.size}`);
 
-          resolve(h.continue);
-        });
+        return h.continue;
       });
     },
   },
